46737216522439039d6f1ff057e86d221a08015f
Shidhu01 <72873562+Shidhu01@users.noreply.github.com>
2022-03-18 20:52:45 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (100.00%) - LeetHub

---
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string smallestSubsequence(string s) {
+        vector<int> freq(26, 0), visited(26, 0);
+        string res = "";
+        
+        for(int i=0; i<s.size(); i++)
+        {
+            freq[s[i] - 'a']++;
+        }
+        
+        for(int i=0; i<s.size(); i++)
+        {
+            
+            freq[s[i] -'a']--;
+            
+            if(!visited[s[i] - 'a'])
+            {
+                while(res.size()>0 and res.back()>s[i] and freq[res.back() - 'a']>0)
+                {
+                    visited[res.back()- 'a'] = 0;
+                    res.pop_back();
+                }
+                res.push_back(s[i]);
+                visited[s[i]-'a'] = 1;
+            }
+        }
+        
+        return res;
+        
+    }
+};
